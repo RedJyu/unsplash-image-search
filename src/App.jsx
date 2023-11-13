6825adf1e6764dc5e12bfa26fb4be70f1ee634a0
RedJyu <piotr.stezycki@protonmai.com>
2023-11-13 12:30:38 +0100

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import DarkModeToggle from './DarkModeToggle';
+import { Gallery } from './Gallery';
+import SearchForm from './SearchForm';
+
 const App = () => {
-  return <h1>Unsplash Images Starter</h1>;
+  return (
+    <main>
+      <DarkModeToggle />
+      <Gallery />
+      <SearchForm />
+    </main>
+  );
 };
 export default App;
---
+++ src/DarkModeToggle.jsx
@@ -0,0 +1,4 @@
+const DarkModeToggle = () => {
+  return <h2>DarkModeToggle</h2>;
+};
+export default DarkModeToggle;
