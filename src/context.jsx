09ef58f5663818c020f43dbfe694ea41aea7ec4e
RedJyu <piotr.stezycki@protonmai.com>
2023-11-16 15:03:32 +0100

    security fixed

--- src/context.jsx
+++ src/context.jsx
@@ -3,7 +3,8 @@ import { createContext, useContext, useState, useEffect } from 'react';
 const AppContext = createContext();
 const getMode = () => {
   const prefersDark = window.matchMedia('(prefers-color-scheme:dark)').matches;
-  return prefersDark;
+  const storedMode = localStorage.getItem('darkTheme') === 'true';
+  return storedMode || prefersDark;
 };
 export const AppProvider = ({ children }) => {
   const [isDark, setIsDark] = useState(getMode());
@@ -11,6 +12,7 @@ export const AppProvider = ({ children }) => {
   const toggleDark = () => {
     const newDark = !isDark;
     setIsDark(newDark);
+    localStorage.setItem('darkTheme', newDark);
   };
   useEffect(() => {
     document.body.classList.toggle('dark-theme', isDark);
