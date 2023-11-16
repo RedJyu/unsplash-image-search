ab9a196faa18a95e024ba08d76b3c168a4da4985
RedJyu <piotr.stezycki@protonmai.com>
2023-11-16 14:49:17 +0100

    fixed dark mode

--- src/context.jsx
+++ src/context.jsx
@@ -1,17 +1,20 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 
 const AppContext = createContext();
-
+const getMode = () => {
+  const prefersDark = window.matchMedia('(prefers-color-scheme:dark)').matches;
+  return prefersDark;
+};
 export const AppProvider = ({ children }) => {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(getMode());
   const [searchTerm, setSearchTerm] = useState('car');
   const toggleDark = () => {
     const newDark = !isDark;
     setIsDark(newDark);
-    const body = document.querySelector('body');
-    body.classList.toggle('dark-theme', newDark);
   };
-
+  useEffect(() => {
+    document.body.classList.toggle('dark-theme', isDark);
+  }, [isDark]);
   return (
     <AppContext.Provider
       value={{ isDark, toggleDark, searchTerm, setSearchTerm }}
